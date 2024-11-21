0106ab00f9b8c06a7423cfa26508e11868fbf0ef
Nathaniel Taulbut <psynt5@nottingham.ac.uk>
2024-11-21 04:39:33 +0000

    format simulator.js with Prettier

--- src/js/simulator.js
+++ src/js/simulator.js
@@ -10,10 +10,10 @@ export function stepForward() {
     const grid = stateModel.cellData;
     let newGrid = [];
 
-    for (let y = 0; y < (grid.length / width); y++) {
+    for (let y = 0; y < grid.length / width; y++) {
         for (let x = 0; x < width; x++) {
-            let neighbourCount = countMooresNeighbours(grid,width,x,y);
-            newGrid.push(computeCellState(grid[x + y * width],neighbourCount));
+            let neighbourCount = countMooresNeighbours(grid, width, x, y);
+            newGrid.push(computeCellState(grid[x + y * width], neighbourCount));
         }
     }
 
@@ -22,19 +22,19 @@ export function stepForward() {
 
 //counts the number of live neighbours within the Moore neighbourhood
 //returns int
-function countMooresNeighbours(grid,width,x,y) {
+function countMooresNeighbours(grid, width, x, y) {
     let sum = 0;
-    let thisIndex = x + (y * width);
-    
-    //itterating through neighbours using index offset
+    let thisIndex = x + y * width;
+
+    //iterating through neighbours using index offset
     for (let offsetY = -1; offsetY < 2; offsetY++) {
         for (let offsetX = -1; offsetX < 2; offsetX++) {
             if (offsetY === 0 && offsetX === 0) {
                 continue;
             }
 
-            let offsetIndex = (thisIndex + offsetX) - (offsetY * width);
-            if (!(offsetIndex < 0 || offsetIndex >= grid.length)) { 
+            let offsetIndex = thisIndex + offsetX - offsetY * width;
+            if (!(offsetIndex < 0 || offsetIndex >= grid.length)) {
                 if (grid[offsetIndex] === 1) {
                     sum++;
                 }
@@ -45,9 +45,12 @@ function countMooresNeighbours(grid,width,x,y) {
 }
 
 //calculate whether a cell should live or die based on total alive neighbours
-function computeCellState(cellState,aliveNeighbours) {
+function computeCellState(cellState, aliveNeighbours) {
     //the below conditional checks for all of the rules that cause a cell to live (Conway's game of life)
-    if ((cellState === 0 && aliveNeighbours === 3) || (cellState === 1) && (aliveNeighbours <= 3 && aliveNeighbours >= 2)) { 
+    if (
+        (cellState === 0 && aliveNeighbours === 3) ||
+        (cellState === 1 && aliveNeighbours <= 3 && aliveNeighbours >= 2)
+    ) {
         return 1;
     }
 
