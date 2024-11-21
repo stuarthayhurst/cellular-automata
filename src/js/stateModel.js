a6c98d139849bc23c02b8a38eb6c8228c1619716
Nathaniel Taulbut <psynt5@nottingham.ac.uk>
2024-11-21 06:10:10 +0000

    add event system to state model

--- src/js/stateModel.js
+++ src/js/stateModel.js
@@ -6,14 +6,19 @@
  * @property {Vec3} cameraDirection - Direction the camera is facing, so it's looking at cameraPosition + cameraDirection.
  * @property {Number} fieldOfView - Field of view of the camera.
  * @property {ViewMode} viewMode - Whether to show the 2D editor or 3D view in the interface.
+ * @property {function(ViewMode)} setViewMode
  * @property {Boolean} paused - If the simulation is paused.
+ * @property {function()} togglePaused
  * @property {Number} baseStepIntervalMillis - Interval in milliseconds between simulation steps taken when not paused.
  * @property {Number} stepIntervalMultiplier - Multiplier applied to the base step interval, e.g. 0.5x, 3x.
+ * @property {function(String, Function)} addEventListener
+ * @property {function(String)} broadcastEvent
  */
 /** @typedef {"2D"|"3D"} ViewMode */
 
 /** @type {StateModel} */
 export const stateModel = {
+    // Cell data
     cellData: [],
 
     // Camera controls
@@ -21,26 +26,30 @@ export const stateModel = {
     cameraDirection: glMatrix.vec3.fromValues(-1.0, -1.0, -1.0),
     fieldOfView: 90.0,
 
+    // View Mode
     viewMode: "3D",
+    setViewMode(newViewMode) {
+        this.viewMode = newViewMode;
+        this.broadcastEvent("onViewModeChanged");
+    },
 
     // Simulation speed controls
     paused: true,
+    togglePaused() {
+        this.paused = !this.paused;
+        this.broadcastEvent("onPausedChanged");
+    },
     baseStepIntervalMillis: 200,
     stepIntervalMultiplier: 1.0,
+
+    // Events system
+    eventListeners: {},
+    addEventListener(eventName, callback) {
+        this.eventListeners[eventName] ??= [];
+        this.eventListeners[eventName].push(callback);
+    },
+    broadcastEvent(eventName) {
+        this.eventListeners[eventName] ??= [];
+        this.eventListeners[eventName].forEach((callback) => callback());
+    },
 };
-
-/**
- * Clear all cells.
- * @param stateModel {StateModel}
- */
-export function clearCells(stateModel) {
-    // TODO: implement this
-}
-
-/**
- * Pause and unpause the simulation.
- * @param stateModel {StateModel}
- */
-export function togglePaused(stateModel) {
-    stateModel.paused = !stateModel.paused;
-}
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,10 +1,17 @@
-import { stateModel, togglePaused } from "./stateModel.js";
+import { stateModel } from "./stateModel.js";
 import { stepForward } from "./simulator.js";
 
 export const canvas = document.querySelector("#renderer");
 
 const pauseButton = document.querySelector("#pause-button");
-pauseButton.addEventListener("click", () => togglePaused(stateModel));
+pauseButton.addEventListener("click", () => stateModel.togglePaused());
+stateModel.addEventListener("onPausedChanged", () => {
+    if (stateModel.paused) {
+        pauseButton.textContent = "Play";
+    } else {
+        pauseButton.textContent = "Pause";
+    }
+});
 
 const stepForwardButton = document.querySelector("#step-forward-button");
 stepForwardButton.addEventListener("click", () => stepForward());
