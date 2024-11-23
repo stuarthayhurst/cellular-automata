a7a76829b6309caf05b2a86ef2c4cde5b922b511
Nathaniel Taulbut <psynt5@nottingham.ac.uk>
2024-11-23 00:14:16 +0000

    organise ui code

---
+++ src/js/ui/controls.js
@@ -0,0 +1,19 @@
+import { stateModel } from "../stateModel.js";
+import { stepForward } from "../simulator.js";
+
+// Pause button
+const pauseButton = document.querySelector("#pause-button");
+pauseButton.addEventListener("click", () => stateModel.togglePaused());
+stateModel.addEventListener("onPausedChanged", () => {
+    if (stateModel.paused) {
+        pauseButton.innerHTML = '<i class="fas fa-play"></i>';
+        pauseButton.title = "Play";
+    } else {
+        pauseButton.innerHTML = '<i class="fas fa-pause"></i>';
+        pauseButton.title = "Pause";
+    }
+});
+
+// Step forward button
+const stepForwardButton = document.querySelector("#step-forward-button");
+stepForwardButton.addEventListener("click", () => stepForward());
