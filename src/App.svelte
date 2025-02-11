0fe6782d201008370c9ae409ee39f4de8c580b91
Xinrui Li <scyxl19@nottingham.ac.uk>
2025-02-11 20:47:36 +0800

    Revert "Feature/add preset functions"
    This reverts commit 2e3dbbffe016643945b1d668a97238489a99c6d4.

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,6 @@
     import Settings from "./lib/components/Settings.svelte";
     import Controls from "./lib/components/Controls.svelte";
     import Display from "./lib/components/Display.svelte";
-    import { sharedState } from "./lib/sharedState.js";
-    import { applyPreset } from "./lib/preset.js";
     import { updateCanvasResolution } from "./lib/components/Display.svelte";
     import { reactiveState } from "./lib/reactiveState.svelte.js";
     import {
@@ -12,7 +10,6 @@
         updateSpeed,
     } from "./lib/simulation.js";
 
-    let presetsList = sharedState.presets;
     let showSettings = $state(false);
     $effect(() => {
         updateCanvasResolution();
@@ -23,15 +20,6 @@
         reactiveState.simulationSpeed;
     });
 </script>
-
-<select
-    onchange={(e) =>
-        applyPreset(e.currentTarget.value, sharedState, reactiveState)}
->
-    {#each Object.keys(presetsList) as key}
-        <option value={key}>{presetsList[key].name}</option>
-    {/each}
-</select>
 
 <main
     class:settings-hidden={!showSettings}
